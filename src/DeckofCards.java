d114e868cfc9e07266c5e8d9901b388d5e70d4cc
KunigiMohan <kmohanr5@gmail.com>
2021-11-13 19:34:54 +0530

    Added : Added DeckofCards.java to create deck of cards.

---
+++ src/DeckofCards.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class DeckofCards {
+    ArrayList<Card> deckofCards = new ArrayList<Card>();
+    public static void main(String[] args){
+        String [] suit = {"club","diamond","heart","Spade"};
+        DeckofCards obj= new DeckofCards();
+        int rank;
+        for(String suits: suit){
+            for(rank=1;rank<=13;rank++)
+            {
+                Card oneCard=new Card(rank);
+                obj.deckofCards.add( oneCard);
+            }
+        }
+    }
+}
